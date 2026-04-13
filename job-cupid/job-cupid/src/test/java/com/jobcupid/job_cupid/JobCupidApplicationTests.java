62c3297df113cb9de3f28015316be2154234a035
TanYuZe <92312650+TanYuZe@users.noreply.github.com>
2026-04-13 22:07:55 +0800

    spring dependencies
    add spring dependencies

---
+++ job-cupid/job-cupid/src/test/java/com/jobcupid/job_cupid/JobCupidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobcupid.job_cupid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobCupidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ job-cupid/src/main/java/com/jobcupid/job_cupid/JobCupidApplication.java
@@ -0,0 +1,13 @@
+package com.jobcupid.job_cupid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobCupidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobCupidApplication.class, args);
+	}
+
+}
